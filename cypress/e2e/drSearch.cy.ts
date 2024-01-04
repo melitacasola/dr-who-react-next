ba8c4b38545950736beee47bf44889229a3c697b
melitacasola <melissacasola@gmail.com>
2024-01-04 23:37:52 +0100

    pruebas de navegacion okgit add .

--- cypress/e2e/drSearch.cy.ts
+++ cypress/e2e/drSearch.cy.ts
@@ -1,9 +1,13 @@
+
+
 describe('template spec', () => {
   it('passes', () => {
     cy.viewport(1728, 1117)
+    cy.clock(Date.now(), ['Date'])
     cy.visit('http://localhost:3000/doctors')
 
     cy.get('#drSearch').type('guerrero', { force: true })
+    cy.wait(1000)
     cy.get('#idSearch').submit()
   })
 })
